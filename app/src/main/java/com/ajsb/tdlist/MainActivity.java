4c613db0190ae449fed7074f57cbea10466be594
bilbobolson <ajsb.ies@gmail.com>
2020-11-23 12:52:20 +0100

    actualización 23/11/2020

--- app/src/main/java/com/ajsb/tdlist/MainActivity.java
+++ app/src/main/java/com/ajsb/tdlist/MainActivity.java
@@ -8,11 +8,16 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private FirebaseAuth fauth ;
+    private FirebaseDatabase fdbase ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +25,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.textView) ;
+        // Obtenemos la instancia de FirebaseAuth y FirebaseDatabase
+        fauth  = FirebaseAuth.getInstance() ;
+        fdbase = FirebaseDatabase.getInstance() ;
+
+        // Obtenemos el UID de nuestro usuario
+        String uid = fauth.getUid() ;
+
+        //
+        DatabaseReference ref = fdbase.getReference("usuarios/"+uid) ;
+        Log.i("REF", ref.toString()) ;
+
+
     }
 }
